03a5020e58dee32713f99cb3e5ef82817f2b24d6
Frenchieoui <frenchieouiouioui@gmail.com>
2026-04-16 10:43:08 -0700

    Correct localStorage initialization and push logic
    Fix syntax errors in localStorage operations.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get("andrewsucks")
 
 if(!localStorage.getItem("andrewsucks"){
-  localStorage.setItem(JSON.stringify({"andrewsucks":[]))
+  localStorage.setItem(JSON.stringify({"andrewsucks":[]}))
 }
 
 const andrewsucks = JSON.parse(localStorage.getItem("andrewsucks"))
