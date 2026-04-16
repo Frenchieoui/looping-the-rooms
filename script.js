0ead95c6502bac306f28541cfb04c0c038b430b9
Frenchieoui <frenchieouiouioui@gmail.com>
2026-04-16 10:39:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get("andrewsucks")
 
+if(!localStorage.getItem("andrewsucks"){
+  localStorage.setItem(JSON.stringify({"andrewsucks":[]))
+}
+
+const andrewsucks = JSON.parse(localStorage.getItem("andrewsucks"))
+andrewsucks.andrewsucks.push(code)
+
+localStorage.setItem("andrewsucks", JSON.stringify(andrewsucks))
